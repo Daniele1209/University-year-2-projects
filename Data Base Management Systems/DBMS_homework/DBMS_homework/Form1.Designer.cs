5baa7ca83280c074e7b0852fd2c18ec720abf61d
Daniele <dany.mosh@yahoo.com>
2021-03-29 15:14:22 +0300

    DBMS hw 1 -> done

---
+++ Data Base Management Systems/DBMS_homework/DBMS_homework/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace DBMS_homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_connect = new System.Windows.Forms.Button();
+            this.shop_grid = new System.Windows.Forms.DataGridView();
+            this.manager_grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.text_name = new System.Windows.Forms.TextBox();
+            this.text_shopId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_update = new System.Windows.Forms.Button();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.text_id = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.shop_grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manager_grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(38, 29);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(196, 45);
+            this.button_connect.TabIndex = 0;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // shop_grid
+            // 
+            this.shop_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.shop_grid.Location = new System.Drawing.Point(38, 106);
+            this.shop_grid.Name = "shop_grid";
+            this.shop_grid.Size = new System.Drawing.Size(457, 450);
+            this.shop_grid.TabIndex = 1;
+            // 
+            // manager_grid
+            // 
+            this.manager_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.manager_grid.Location = new System.Drawing.Point(731, 106);
+            this.manager_grid.Name = "manager_grid";
+            this.manager_grid.Size = new System.Drawing.Size(344, 450);
+            this.manager_grid.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "SHOP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(866, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "SHOP MANAGER";
+            // 
+            // text_name
+            // 
+            this.text_name.Location = new System.Drawing.Point(581, 154);
+            this.text_name.Name = "text_name";
+            this.text_name.Size = new System.Drawing.Size(126, 20);
+            this.text_name.TabIndex = 7;
+            // 
+            // text_shopId
+            // 
+            this.text_shopId.Location = new System.Drawing.Point(581, 202);
+            this.text_shopId.Name = "text_shopId";
+            this.text_shopId.Size = new System.Drawing.Size(126, 20);
+            this.text_shopId.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(526, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Shop_id:";
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(567, 244);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(104, 40);
+            this.button_add.TabIndex = 11;
+            this.button_add.Text = "Add";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_update
+            // 
+            this.button_update.Location = new System.Drawing.Point(567, 321);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(104, 40);
+            this.button_update.TabIndex = 12;
+            this.button_update.Text = "Update";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(567, 393);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(104, 40);
+            this.button_delete.TabIndex = 13;
+            this.button_delete.Text = "Delete";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // text_id
+            // 
+            this.text_id.Location = new System.Drawing.Point(581, 106);
+            this.text_id.Name = "text_id";
+            this.text_id.Size = new System.Drawing.Size(126, 20);
+            this.text_id.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(553, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Id: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 590);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.text_id);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.button_update);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.text_shopId);
+            this.Controls.Add(this.text_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.manager_grid);
+            this.Controls.Add(this.shop_grid);
+            this.Controls.Add(this.button_connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.shop_grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.manager_grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.DataGridView shop_grid;
+        private System.Windows.Forms.DataGridView manager_grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox text_name;
+        private System.Windows.Forms.TextBox text_shopId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.TextBox text_id;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Data Base Management Systems/DBMS_homework/DBMS_homework/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBMS_homework
+{
+    public partial class Form1 : Form
+    {
+
+        private SqlConnection connection = new SqlConnection("Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True");
+        private SqlDataAdapter shopTable;
+        private DataSet shopSet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            shop_grid.SelectionChanged += new EventHandler(loadChildren);
+            loadParent();
+
+        }
+
+        private void loadChildren(object sender, EventArgs e)
+        {
+            LoadChildren();
+        }
+
+        private void LoadChildren()
+        {
+            try
+            {
+                int shop_id = Convert.ToInt32(shop_grid.CurrentRow.Cells[0].Value);
+                SqlCommand command = new SqlCommand("SELECT * FROM ShopManager WHERE Shop_id=@id")
+                {
+                    Connection = new SqlConnection("Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True")
+                };
+                command.Parameters.AddWithValue("@id", shop_id);
+
+                shopTable = new SqlDataAdapter(command);
+                shopSet = new DataSet();
+
+                shopTable.Fill(shopSet, "ShopManager");
+                manager_grid.DataSource = shopSet.Tables["ShopManager"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void loadParent()
+        {
+            SqlDataAdapter shopTable = new SqlDataAdapter("SELECT * FROM Shop", "Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True");
+            DataSet dataSet = new DataSet();
+
+            shopTable.Fill(dataSet, "Shop");
+            shop_grid.DataSource = dataSet.Tables["Shop"];
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True");
+            SqlDataReader reader = null;
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Connection Open!");
+                SqlCommand command = new SqlCommand("select count(*) from Shop", conn);
+                reader = command.ExecuteReader();
+                reader.Read();
+                MessageBox.Show(reader[0].ToString());
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+
+                conn.Close();
+            }
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True");
+            try
+            {
+                SqlDataAdapter shopTable = new SqlDataAdapter("SELECT * FROM Shop", "Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True");
+                SqlCommand InsertCommand = new SqlCommand("INSERT INTO ShopManager (Name,Shop_id) VALUES (@n, @id)", conn);
+
+                InsertCommand.Parameters.Add("@n", SqlDbType.VarChar).Value = text_name.Text;
+                InsertCommand.Parameters.Add("@id", SqlDbType.Int).Value = Convert.ToInt32(text_shopId.Text);
+
+
+                shopTable = new SqlDataAdapter(InsertCommand);
+                shopSet = new DataSet();
+
+                conn.Open();
+                InsertCommand.ExecuteNonQuery();
+                conn.Close();
+                MessageBox.Show("Manager inserted successfully !");
+
+                LoadChildren();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conn.Close();
+            }
+        }
+
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True");
+            try
+            {
+                SqlDataAdapter shopTable = new SqlDataAdapter("SELECT * FROM Shop", "Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True");
+                SqlCommand UpdateCommand = new SqlCommand("UPDATE ShopManager SET Name=@n, Shop_id=@id WHERE Smid=@sid", conn);
+
+                UpdateCommand.Parameters.Add("@sid", SqlDbType.Int).Value = Convert.ToInt32(text_id.Text);
+                UpdateCommand.Parameters.Add("@n", SqlDbType.VarChar).Value = text_name.Text;
+                UpdateCommand.Parameters.Add("@id", SqlDbType.Int).Value = Convert.ToInt32(text_shopId.Text);
+
+                shopTable = new SqlDataAdapter(UpdateCommand);
+
+                conn.Open();
+                UpdateCommand.ExecuteNonQuery();
+                conn.Close();
+
+                LoadChildren();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conn.Close();
+            }
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True");
+            try
+            {
+                SqlDataAdapter shopTable = new SqlDataAdapter("SELECT * FROM Shop", "Data Source=DESKTOP-QSLMK25\\SQLEXPRESS;Initial Catalog=Video_Games;Integrated Security=True");
+                SqlCommand UpdateCommand = new SqlCommand("DELETE FROM ShopManager WHERE Smid=@sid", conn);
+
+                UpdateCommand.Parameters.Add("@sid", SqlDbType.Int).Value = Convert.ToInt32(text_id.Text);
+                shopTable = new SqlDataAdapter(UpdateCommand);
+
+                conn.Open();
+                UpdateCommand.ExecuteNonQuery();
+                conn.Close();
+
+                LoadChildren();
+                MessageBox.Show("Manager deleted successfully !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conn.Close();
+            }
+        }
+    }
+}
